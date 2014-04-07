eb881bff65d223d9ad096b5ec60d17d12a4ebee0
slybouze <slybouze@frogswarm.fr>
2014-04-08 00:09:35 +0200

    parsing du json, récupération de la session en cours, calcul des dates de début et de fin

--- simulator/raspi.js
+++ simulator/raspi.js
@@ -3,8 +3,43 @@ var app = express();
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(__dirname + '/votes.db');
-//var db = new sqlite3.Database(':memory:');
+
+var jsonPath = require('JSONPath');
+
 var i = 0;
+
+var jourCourant = '13/06/2013';
+var heureCourante = '9:45';
+var salleCourante = 'Groix';
+
+var programJSON = require(__dirname + '/breizhcamp.json');
+
+var confsJour = jsonPath.eval(programJSON, "$..jours[?(@.date=='" + jourCourant + "')]");
+var confsJourSalle = jsonPath.eval(confsJour, "$..proposals[?(@.room=='" + salleCourante + "')]");
+for (var conf in confsJourSalle) {
+	var debut = confsJourSalle[conf].time.split(':');
+	confsJourSalle[conf].start = parseInt(debut[0]*60)+parseInt(debut[1]);
+	switch (confsJourSalle[conf].format) {
+	case 'quickie' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 40;
+	case 'tools in action' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 25;
+	case 'hands-on' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 115;
+	case 'conference' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 55;
+	case 'lab' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 85;
+	case 'biglab' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 115;
+	case 'universite' : 
+		confsJourSalle[conf].stop = parseInt(confsJourSalle[conf].start) + 180;
+	}
+} 
+
+var heureCourante = 600;
+var confCourante = jsonPath.eval(confsJourSalle, "$[?(@.start<'" + heureCourante + "'&&@.stop>'" + heureCourante + "')]");
+var sessiontext=confCourante[0].title;
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
@@ -13,7 +48,7 @@ app.get('/', function(req, res) {
 
 app.get('/text', function(req, res) {
     db.get("SELECT sum(vote) as total, count(vote) as nbvote FROM votes WHERE sessionId='1'", function (error, row) { 
-	var sessiontext = 'Developper en Java et en caleçon';
+	//var sessiontext = 'Developper en Java et en caleçon';
 	var sessiontextdisp = sessiontext.concat('---');
 	var progressbar = ':( |------------| :)';
 	var total = row.total; 
@@ -21,7 +56,7 @@ app.get('/text', function(req, res) {
 	var curseur = Math.round(total*12/votes);
 	console.log(row);
     	res.setHeader('Content-Type', 'text/plain');
-    	res.end(sessiontextdisp.substr(i,20) + sessiontextdisp.substr(0,i-sessiontextdisp.length+20) + '<BR/>&nbsp;12h30&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;13h30&nbsp;<BR/>Temps vote : -12min<BR/>' + setCharAt(progressbar,curseur+4,'*'));
+    	res.end(sessiontextdisp.substr(i,20) + sessiontextdisp.substr(0,i-sessiontextdisp.length+20) + '<BR/>&nbsp;' + Math.floor(parseInt(confCourante[0].start)/60) + 'h' + deuxchiffres(parseInt(confCourante[0].start) % 60)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(parseInt(confCourante[0].stop)/60) + 'h' + deuxchiffres(parseInt(confCourante[0].stop) % 60) + '&nbsp;<BR/>Temps vote : -12min<BR/>' + setCharAt(progressbar,curseur+4,'*'));
 	if (i == sessiontextdisp.length) { i = 0; } else { i = i+1; };
     	console.log('Acces text ' + i);
 	}
@@ -41,4 +76,8 @@ console.log('Server running on 8088 port');
 function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+function deuxchiffres(n){
+    return n > 9 ? "" + n: "0" + n;
 }
