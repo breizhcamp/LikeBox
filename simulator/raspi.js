d915f241dfe1561132ad3c6d80a2d462d173bbb6
slybouze <slybouze@frogswarm.fr>
2014-04-08 23:32:47 +0200

    câblage du vote avec le numéro de session

--- simulator/raspi.js
+++ simulator/raspi.js
@@ -12,10 +12,16 @@ var i = 0;
 var jourCourant = '13/06/2013';
 var heureCourante = '9:45';
 var salleCourante = 'Groix';
+var sessionNum = '999'; 
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
-    console.log('Acces racine');
+    	res.sendfile(__dirname + '/index.html');
+    	console.log('Acces racine');
+});
+
+app.get('/sessionId', function(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end(sessionNum.toString());
 });
 
 app.get('/text', function(req, res) {
@@ -61,16 +67,18 @@ app.get('/text', function(req, res) {
 	// Recherche de la session correspondant à l'heure courante
 	var heureCouranteSplit = heureCourante.split(':');
 	var heureCouranteNum = parseInt(heureCouranteSplit[0]*60)+parseInt(heureCouranteSplit[1]);
-	var confCourante = jsonPath.eval(confsJourSalle, "$[?(@.start<'" + heureCouranteNum + "'&&@.stop>'" + heureCouranteNum + "')]");
+	var confCourante = jsonPath.eval(confsJourSalle, "$[?(@.start<'" + heureCouranteNum + "'&&@.finvote>'" + heureCouranteNum + "')]");
 
 	// Si il n'y a pas de session en cours, on affiche un petit mot...
 	if (confCourante.length == 0) { 
+		sessionNum = '999';
 		res.setHeader('Content-Type', 'text/plain');
 		res.end('BreizhCamp<BR/>For<BR/>The<BR/>Win');
 	} else {
 	// Si on trouve la session, on affiche les informations sur le LCD
 		var sessiontext=confCourante[0].title;
-		var sessionNum=confCourante[0].id;
+		sessionNum=confCourante[0].id;
+		console.log(sessionNum);
 		// Ligne 1 - Génération de la rotation sur le titre de la session en cours - TODO : c'est ptet un peu buggé...
 		var sessiontextdisp = sessiontext.concat('---');
 		var rotatingText = sessiontextdisp.substr(i,20) + sessiontextdisp.substr(0,i-sessiontextdisp.length+20);
@@ -79,15 +87,16 @@ app.get('/text', function(req, res) {
 		var heuresConf = Math.floor(parseInt(confCourante[0].start)/60) + 'h' + deuxchiffres(parseInt(confCourante[0].start) % 60)  + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(parseInt(confCourante[0].stop)/60) + 'h' + deuxchiffres(parseInt(confCourante[0].stop) % 60);
 
 		// Ligne 3 - Affichage du temps avant la fin des votes
-		var tempsFinVote = (parseInt(confCourante[0].finvote)) - parseInt(heureCourante);
+		var tempsFinVote = (parseInt(confCourante[0].finvote)) - parseInt(heureCouranteNum);
 
 		// Récupération des votes pour la session en cours
-		db.get("SELECT sum(vote) as total, count(vote) as nbvote FROM votes WHERE sessionId='1'", function (error, row) { 
+		db.get("SELECT sum(vote) as total, count(vote) as nbvote FROM votes WHERE sessionId='" + sessionNum + "'", function (error, row) { 
 			// Ligne 4 - Affichage du vote sur une barre de progression - TODO : virer la division par zéro si il n'y a pas de votes :P
 			var progressbar = ':( |------------| :)';
 			var total = row.total; 
 			var votes = row.nbvote; 
-			var curseur = Math.round(total*12/votes);
+			var curseur = 6;
+			if (votes != 0) { curseur = Math.round(total*12/votes); };
 			// Positionnement du curseur de vote sur la barre
 			var progressbarCurseur = setCharAt(progressbar,curseur+4,'*');
 			console.log(row);
@@ -102,24 +111,28 @@ app.get('/text', function(req, res) {
 	};
 });
 
-app.get('/vote/:valeur', function(req, res) {
-    var stmt = db.prepare("INSERT INTO votes ('sessionId', 'vote', 'timeStamp') values ($session, $vote, datetime('now'))");
-    session='1';
-    stmt.run({$session : session, $vote : req.params.valeur});
-    console.log('Vote pour la session' + session + ' : ' + req.params.valeur);
+//app.get('/vote/:valeur', function(req, res) {
+//    var stmt = db.prepare("INSERT INTO votes ('sessionId', 'vote', 'timeStamp') values ($session, $vote, datetime('now'))");
+//    session='1';
+//    stmt.run({$session : session, $vote : req.params.valeur});
+//    console.log('Vote pour la session' + session + ' : ' + req.params.valeur);
+//});
+
+app.post('/vote', function(req, res) {
+    	var stmt = db.prepare("INSERT INTO votes ('sessionId', 'vote', 'timeStamp') values ($session, $vote, datetime('now'))");
+    	var session = req.body.session;
+    	stmt.run({$session : session, $vote : req.body.valeur});
+	console.log('Vote pour la session' + session + ' : ' + req.body.valeur);
+	res.send("ok");
 });
 
 app.post('/settime', function(req, res) {
-	console.log("Set time : ");
-	console.log(req.body);
 	jourCourant=req.body.jour;
 	heureCourante= req.body.heure + ':' + req.body.minutes;
 	res.send("ok");
 });
 
 app.post('/setsalle', function(req, res) {
-	console.log("Set Salle : ");
-	console.log(req.body);
 	salleCourante = req.body.Choix;
 	res.send("ok");
 });
