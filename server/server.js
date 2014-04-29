a2bc03228ec95232b82dce521336191268751c7a
srevereault <sylvain@revereault.fr>
2014-04-29 18:12:53 +0200

    Début de l'implémentation serveur

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(__dirname + '/server_votes.db');
 
 app.get('/', function(req, res) {
 	res.send('It works');
+});
+
+app.get('/Program', function(req, res) {
+        res.download(__dirname + '/schedule.json');
+});
+
+app.post('/Vote/:idsession', function(req, res) {
+        var stmt = db.prepare("INSERT INTO votes ('sessionId', 'vote', 'timeStamp') values ($session, $vote, datetime('now'))");
+        var session = req.params.idsession;
+	console.log(req.body);
+        stmt.run({$session : session, $vote : req.body.valeur});
+        console.log('Vote pour la session ' + session + ' : ' + req.body.valeur);
+        res.send("ok");
 });
 
 app.listen(3000, function() {
