944a7546fbd4a41ab66e60c27a12b4ac6e622d1e
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-10-16 14:39:04 +0200

    Use sched.org JSON format so we can easily update

--- client/modules/schedule.js
+++ client/modules/schedule.js
@@ -8,41 +8,6 @@ var fs = require('fs'),
  */
 module.exports = function(scheduleFile) {
 	return {
-		/**
-		 * Returns all rooms names from the schedule.
-		 * @returns {promise|Q.promise} Param will contains an array with rooms name
-		 */
-		getRooms: function() {
-			var deferred = Q.defer();
-			fs.readFile(scheduleFile, 'utf8', function (err, data) {
-				if (err) {
-					deferred.reject(new Error(err));
-					return;
-				}
-				var schedule = JSON.parse(data);
-				var rooms = {};
-
-				var days = schedule.programme.jours;
-				for (var i = 0 ; i < days.length ; i++) {
-					var proposals = days[i].proposals;
-
-					for (var k = 0 ; k < proposals.length ; k++) {
-						var room = proposals[k].room;
-						if (room.trim().length > 0) {
-							rooms[room] = room;
-						}
-					}
-				}
-
-				//build the final array
-				var res = [];
-				Object.keys(rooms).forEach(function(key) {
-					res.push(key);
-				});
-				deferred.resolve(res);
-			});
-			return deferred.promise;
-		},
 
 		/**
 		 * Retrieve the current voting session from the schedule file. The voting starts 10 minutes
@@ -65,42 +30,25 @@ module.exports = function(scheduleFile) {
 
 				var schedule = JSON.parse(data);
 				var now = moment();
-
-				//let's find the current day in schedule file
 				var current;
-				var days = schedule.programme.jours;
-				for (var i = 0 ; i < days.length ; i++) {
-					var date = days[i].date; //ex: 21/05/2014
-					var day = moment(date, "DD/MM/YYYY");
-					if (!day.isSame(now, 'day')) {
+
+				for (var k = 0 ; k < schedule.length ; k++) {
+					var event = schedule[k];
+					if (event.venue_id != room) {
 						continue;
 					}
 
-					//we're on the good day, let's find the right proposal
-					var proposals = days[i].proposals;
+					var start = moment(event.event_start, 'YYYY-MM-DD HH:mm:ss')
+							.add(startDelay, 'minutes'),
+						end = moment(event.event_end, 'YYYY-MM-DD HH:mm:ss')
+							.add(endDelay, 'minutes');
 
-					for (var k = 0 ; k < proposals.length ; k++) {
-						var proposal = proposals[k];
-						if (proposal.room != room) {
-							continue;
-						}
-
-						//we're on the right room, let's check the start and end+delay
-						var start = moment(date + ' ' + proposal.start, 'DD/MM/YYYY HH:mm:ss')
-								.add(startDelay, 'minutes'),
-							end = moment(date + ' ' + proposal.end, 'DD/MM/YYYY HH:mm:ss')
-									.add(endDelay, 'minutes');
-
-						if (start.isBefore(now) && end.isAfter(now)) {
-							//we found the current proposal
-							current = proposal;
-							current.endDate = end;
-
-						}
+					if (start.isBefore(now) && end.isAfter(now)) {
+						//we found the current event
+						current = event;
+						current.endDate = end;
+						break;
 					}
-
-					//on the right day, exit loop
-					break;
 				}
 
 				//if we don't hit any current session, we return nothing
