3c532158123be76fbbd430fe2442a552c88e3471
Laurent HUET <laurent.huet@gmail.com>
2015-11-08 21:59:15 +0100

    Add install.sh to complete hypriot-image

---
+++ docker/install.sh
@@ -0,0 +1,14 @@
+echo "rtc-ds1307" >> /etc/modules
+echo "i2c-dev" >> /etc/modules
+echo "i2c-bcm2708" >> /etc/modules
+
+sed -i '/exit 0/d' /etc/rc.local
+echo "echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device" >> /etc/rc.loc
+al
+echo "hwclock -s" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+
+echo "dtparam=i2c1=on" >> /boot/config.txt
+echo "dtparam=i2c_arm=on" >> /boot/config.txt
+apt-get update
+apt-get install i2c-tools
