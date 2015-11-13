963b02889dad81fa69add753cce05ac7294954e9
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-11-13 10:42:05 +0100

    fix cpuinfo

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ -z $BOX_ID ]]; then
     echo "BOX_ID unset, using cpuinfo Serial"
-    export BOX_ID=`cat /proc/cpuinfo|grep Serial|cut -c 13-99`
+    export BOX_ID=`cat /proc/cpuinfo|grep Serial|cut -c 11-99`
 fi
 echo "BOX_ID=$BOX_ID"
 exec env "BOX_ID=$BOX_ID" "$@"
