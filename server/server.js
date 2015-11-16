06965b167e3c1d0c8094af7b93a3c6793d3ec4cb
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-11-16 12:10:00 +0100

    can’t use aggregate on where condition

--- server/server.js
+++ server/server.js
@@ -169,7 +169,7 @@ server.get('/top/:nb', function(req, res, next) {
     var row_num = 1;
     db.each("SELECT sessionId, nb_votes, (somme/nb_votes) as average from " +
         "(SELECT sessionId, sum(vote) as somme, count(vote) as nb_votes " +
-        "from votes where count(vote) > 0 group by sessionId) order by average desc, nb_votes desc limit " + nombre , function (error, row) {
+        "from votes group by sessionId) order by average desc, nb_votes desc limit " + nombre , function (error, row) {
 
         var sessionId = row.sessionId;
         var cur_sess = row_num;
